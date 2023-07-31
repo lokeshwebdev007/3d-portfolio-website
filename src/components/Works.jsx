34f92b3e4dd78cabc0fef2b2cd2046db9e3b7dca
lokesh sharma <lokeshsharma292001@gmail.com>
2023-07-31 13:37:33 +0530

    first

---
+++ src/components/Works.jsx
@@ -0,0 +1,89 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { github } from "../assets";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[350px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute top-0 right-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-5 h-5 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+        <div className="flex flex-wrap gap-2 mt-4">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`${tag.color} text-[14px]`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.2, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[25px]"
+        >
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem
+          totam id autem doloribus atque libero velit eos vitae. Nihil
+          doloremque qui dolorum mollitia repudiandae voluptatum maiores.
+          Ratione laudantium veritatis eveniet deserunt id ipsa et reiciendis
+          rerum neque, numquam nam sunt, quo eos culpa architecto nesciunt odit
+          fuga laboriosam. Animi numquam architecto quos facere perspiciatis
+          iusto? Quia iste officia incidunt fugit?
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7 ">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "works");
